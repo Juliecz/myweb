2baefec847492dada8f875301b640385e13d513a
juliecz <yuliya@yuliya.cz>
2017-12-14 16:22:38 +0100

    Add links to projects

--- src/components/partials/Project/Project.js
+++ src/components/partials/Project/Project.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import * as images from './assets/index';
+import Image from './Image';
 
 import './style.css';
 
-const MyProjects = ({ src, alt, name, description }) => (
+const MyProjects = ({ src, alt, url, name, description }) => (
 	<div className="project">
-		<a href="">
-			<img
-				src={images[src]}
-				alt={alt}
-			/>
-		</a>
+		{url
+			? <a href={url} target="_blank">
+				<Image src={src} alt={alt} />
+			</a>
+			: <Image src={src} alt={alt} />
+		}
 		<h4>{name.toUpperCase()}</h4>
 		<p>{description}</p>
 	</div>
